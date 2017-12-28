26632aed8cb5c609c3af5a7f23c8ca86f078db1c
Y0hy0h <Y0hy0h@users.noreply.github.com>
2017-12-28 13:42:25 +0100

    Add stubs for missing tests for `ExpenseInput`

--- test/expense_overview/expense_input_test.dart
+++ test/expense_overview/expense_input_test.dart
@@ -5,24 +5,46 @@ import "package:plus_minus/expense_storage.dart";
 
 void main() {
   group("ExpenseInput", () {
-    testWidgets("parses valid input", (WidgetTester tester) async {
-      ExpenseInput _expenseInput = new ExpenseInput();
-      await tester.pumpWidget(
-        new MaterialApp(
-          home: new Material(child: _expenseInput),
-        ),
+    ExpenseInput expenseInput;
+    Widget renderableWrapper;
+
+    // TODO: Make TextField finding more robust by referencing their hints.
+    Finder amountTextField = find.byType(TextField).first;
+    Finder categoryTextField = find.byType(TextField).last;
+
+    setUp(() async {
+      expenseInput = new ExpenseInput();
+      renderableWrapper = new MaterialApp(
+        home: new Material(child: expenseInput),
       );
+    });
 
-      // TODO: Make TextField finding more robust by referencing their hints.
-      await tester.enterText(find.byType(TextField).first, "12345");
-      await tester.enterText(find.byType(TextField).last, "Banana");
+    testWidgets("parses valid input", (WidgetTester tester) async {
+      await tester.pumpWidget(renderableWrapper);
 
-      Expense parsedExpense = _expenseInput.parseExpense();
-      Duration dateDifference = parsedExpense.date.difference(new DateTime.now());
+      await tester.enterText(amountTextField, "12345");
+      await tester.enterText(categoryTextField, "Banana");
+
+      Expense parsedExpense = expenseInput.parseExpense();
+      Duration dateDifference =
+          parsedExpense.date.difference(new DateTime.now());
 
       expect(parsedExpense.amount, equals(123.45));
       expect(parsedExpense.description, equals("Banana"));
       expect(dateDifference.inSeconds, lessThan(1));
     });
+
+    testWidgets('throws error on invalid date', (WidgetTester tester) async {
+      await tester.pumpWidget(renderableWrapper);
+    }, skip: true);
+
+    testWidgets('accepts empty description', (WidgetTester tester) async {
+      await tester.pumpWidget(renderableWrapper);
+    }, skip: true);
+
+    testWidgets('calls submit callback on keyboard submit',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(renderableWrapper);
+    }, skip: true);
   });
 }
